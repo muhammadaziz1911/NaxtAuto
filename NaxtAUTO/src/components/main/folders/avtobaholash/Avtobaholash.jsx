1cb394deed510dd84328ee8a480497188d2760eb
muhammadaziz1911 <azizbekmaxmudov22@gmail.com>
2026-05-14 22:56:07 +0500

    update

--- NaxtAUTO/src/components/main/folders/avtobaholash/Avtobaholash.jsx
+++ NaxtAUTO/src/components/main/folders/avtobaholash/Avtobaholash.jsx
@@ -201,6 +201,12 @@ const Avtobaholash = () => {
                 </h3>
               </div>
 
+              <div className="edit_button">
+                <h3>
+                  Edit
+                </h3>
+              </div>
+
             </div>
 
         </div>
