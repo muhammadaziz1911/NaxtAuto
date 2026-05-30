04a37c73a936fbf83101efcc34bd2800178cf2ce
muhammadaziz1911 <azizbekmaxmudov22@gmail.com>
2026-05-30 23:37:02 +0500

    update

--- NaxtAUTO/src/components/main/folders/arizalar/Arizalar.jsx
+++ NaxtAUTO/src/components/main/folders/arizalar/Arizalar.jsx
@@ -16,22 +16,12 @@ const Arizalar = () => {
         You can't read this page now!
       </h4>
 
-      <input type="text" placeholder='Enter you name' />
-      <input type="password" placeholder='Password' />
+      <div className="new_section">
+        <h4>
+          Yangi bob
+        </h4>
+      </div>
 
-      <input type="text" placeholder='Enter you name' />
-      <input type="password" placeholder='Password' />
-
-      <input type="text" placeholder='Enter you name' />
-      <input type="password" placeholder='Password' />
-
-
-      <input type="text" placeholder='Enter you name' />
-      <input type="password" placeholder='Password' />
-
-
-      <input type="text" placeholder='Enter you name' />
-      <input type="password" placeholder='Password' />
     </div>
   )
 }
