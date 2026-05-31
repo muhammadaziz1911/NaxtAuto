61f04eeecd58f033437c3bd42fcd3d061386d272
muhammadaziz1911 <azizbekmaxmudov22@gmail.com>
2026-05-31 22:59:49 +0500

    update

---
+++ NaxtAUTO/src/components/main/folders/marketplace/Marketplace.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Marketplace = () => {
+  return (
+    <div>
+      
+      <div className="marketplace_cars">
+
+        <div className="cars_market">
+
+          <h3>
+            Avtomobillar bozori
+          </h3>
+
+          <div className="boxes">
+            <img src="" alt="car" />
+            <p>Gentra</p>
+          </div>
+          <div className="boxes">
+            <img src="" alt="car" />
+            <p>Gentra</p>
+          </div>
+          <div className="boxes">
+            <img src="" alt="car" />
+            <p>Gentra</p>
+          </div>
+          <div className="boxes">
+            <img src="" alt="car" />
+            <p>Gentra</p>
+          </div>
+          <div className="boxes">
+            <img src="" alt="car" />
+            <p>Gentra</p>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Marketplace
