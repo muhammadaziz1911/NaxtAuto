2d0f72388ef50f34c9ba75554c5ad7a325d82e98
muhammadaziz1911 <azizbekmaxmudov22@gmail.com>
2026-06-06 22:52:05 +0500

    update

--- NaxtAUTO/src/components/main/folders/marketplace/Marketplace.jsx
+++ NaxtAUTO/src/components/main/folders/marketplace/Marketplace.jsx
@@ -43,6 +43,11 @@ const Marketplace = () => {
             <p>Gentra</p>
           </div>
 
+          <div className="boxes">
+            <img src="" alt="car" />
+            <p>Gentra</p>
+          </div>
+
         </div>
 
       </div>
